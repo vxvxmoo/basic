8a01690b36caeb722d6a05151d7500eb07165025
mikan <joooong1021@gmail.com>
2023-12-12 10:24:24 +0900

    add: 배열 상태 관리

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import AppXY from "./AppXY";
-import AppMentor from "./AppMentor";
+import AppMentors from "./AppMentors";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppMentor />
+    <AppMentors />
   </React.StrictMode>
 );
 
